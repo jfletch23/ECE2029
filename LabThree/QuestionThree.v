ea9a64ae45c2c66da113ccf641ef8a205d779589
Justin Fletcher <123443555+jfletch23@users.noreply.github.com>
2025-11-13 11:57:07 -0500

    Add files via upload

--- LabThree/QuestionThree.v
+++ LabThree/QuestionThree.v
@@ -25,18 +25,21 @@ module QuestionThree(
     input [2:0]B,
     input M,
     output [2:0]D,
-    output Carry
+    output Carry,
+    output V
     );
-    wire [2:0]c;
-    wire inputOne;
-    wire inputTwo;
-    wire inputThree;
-    assign inputOne = B[0] ^ M;
-    assign inputTwo = B[1] ^ M;
-    assign inputThree = B[2] ^ M;
-    QuestionOneDesignSource f1(.X(A[0]),.Y(inputOne),.Z(1),.Sum(D[0]),.Carry(c[0]));
-    QuestionOneDesignSource f2(.X(A[1]),.Y(inputTwo),.Z(c[0]),.Sum(D[1]),.Carry(c[1]));
-    QuestionOneDesignSource f3(.X(A[2]),.Y(inputThree),.Z(c[1]),.Sum(D[2]),.Carry(c[2]));
-    assign Carry = c[2];
+//    wire [2:0]c;
+    wire c0, c1, c2;
+    wire inputOne = B[0] ^ M;
+    wire inputTwo = B[1] ^ M;
+    wire inputThree = B[2] ^ M;
+//    assign inputOne = B[0] ^ M;
+//    assign inputTwo = B[1] ^ M;
+//    assign inputThree = B[2] ^ M;
+    QuestionOneDesignSource f1(.X(A[0]),.Y(inputOne),.Z(M),.Sum(D[0]),.Carry(c0));
+    QuestionOneDesignSource f2(.X(A[1]),.Y(inputTwo),.Z(c0),.Sum(D[1]),.Carry(c1));
+    QuestionOneDesignSource f3(.X(A[2]),.Y(inputThree),.Z(c1),.Sum(D[2]),.Carry(c2));
+    assign V = c1 ^ c2;
+    assign Carry = (~V & c2);
     
 endmodule
--- LabThree/QuestionThreeTestBench.v
+++ LabThree/QuestionThreeTestBench.v
@@ -28,17 +28,65 @@ module QuestionThreeTestBench(
     reg M;
     wire [2:0]D;
     wire c;
+    wire V;
     //In the parenthesis is from the current file
     //Next to the period is from the referenced file
-    QuestionThree dut(.A(A),.B(B),.M(M),.D(D),.Carry(c));
+    QuestionThree dut(.A(A),.B(B),.M(M),.D(D),.Carry(c),.V(V));
     initial begin
     A[0]=1;
     B[0]=1;
     A[1]=1;
     B[1]=0;
-    A[2]=0;
+    A[2]=1;
     B[2]=1;
-    M=0;
+    M=1;
     #10;
+    A[0]=1;
+    B[0]=1;
+    A[1]=1;
+    B[1]=0;
+    A[2]=1;
+    B[2]=0;
+    M=1;
+    #10;
+    A[0]=1;
+    B[0]=0;
+    A[1]=1;
+    B[1]=1;
+    A[2]=1;
+    B[2]=0;
+    M=1;
+    #10;
+    A[0]=1;
+    B[0]=1;
+    A[1]=1;
+    B[1]=1;
+    A[2]=1;
+    B[2]=0;
+    M=1;
+    #10;
+    A[0]=1;
+    B[0]=0;
+    A[1]=1;
+    B[1]=0;
+    A[2]=1;
+    B[2]=1;
+    M=1;
+    #10;
+    A[0]=0;
+    B[0]=1;
+    A[1]=0;
+    B[1]=1;
+    A[2]=1;
+    B[2]=0;
+    M=1;
+    #10;
+    A[0]=0;
+    B[0]=1;
+    A[1]=0;
+    B[1]=1;
+    A[2]=1;
+    B[2]=0;
+    M=0;                        
     end
 endmodule
